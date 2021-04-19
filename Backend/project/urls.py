2f6fd9e414163758f621555daefc46eed6a8abca
Aida Bourdis <aida.lucie@gmail.com>
2021-04-19 10:42:34 +0100

    fixed no module name jwt.auth error

--- Backend/project/urls.py
+++ Backend/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/potions/', include('potions.urls')),
-    path('api/auth/', include('jwt.auth.urls'))
+    path('api/auth/', include('jwt_auth.urls'))
 ]
