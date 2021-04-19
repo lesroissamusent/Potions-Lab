c765edb952155bd3df7fed391b2fe852dcc4f262
Aida Bourdis <aida.lucie@gmail.com>
2021-04-19 12:33:39 +0100

    register and login working, but email isn't getting saved for some reason, updating manuallyin tableplus makes it work

--- Backend/jwt_auth/urls.py
+++ Backend/jwt_auth/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view())
 ]
--- Backend/project/urls.py
+++ Backend/project/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/potions/', include('potions.urls')),
     path('api/auth/', include('jwt_auth.urls'))
 ]
